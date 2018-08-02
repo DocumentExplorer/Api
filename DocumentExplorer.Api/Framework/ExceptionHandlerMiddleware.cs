17491c24ea963f65a1683846ad8a4bca1ef082fa
LukaszKot <lukaszkot03@gmail.com>
2018-08-02 20:00:05 +0200

    fixed logs

--- DocumentExplorer.Api/Framework/ExceptionHandlerMiddleware.cs
+++ DocumentExplorer.Api/Framework/ExceptionHandlerMiddleware.cs
@@ -52,6 +52,7 @@ namespace DocumentExplorer.Api.Framework
                     break;
             }
             Logger.Log(NLog.LogLevel.Error, exception, exception.StackTrace);
+            Logger.Log(NLog.LogLevel.Error, exception, exception.Message, exception.StackTrace);
             var response = new { code = errorCode };
             var payload = JsonConvert.SerializeObject(response);
             context.Response.ContentType = "application/json";
